470d2cbb5fd83cf13ceeb757415469140edf02bc
Thomas Lacroix <thomas.am.lacroix@gmail.com>
2026-05-11 13:24:38 +0200

    solved issue with reconstructed image

--- color/interface/main.py
+++ color/interface/main.py
@@ -3,13 +3,16 @@ Main to test predictions before implementing API endpoint
 """
 
 from color.registry import load_model
-# from color.params import IMAGE_SIZE
+from color.params import IMAGE_SIZE
 from color.utils import resize_image, rgb_to_lab
 import numpy as np
 from PIL import Image
 import tensorflow as tf
 import tensorflow_io as tfio
 import matplotlib.pyplot as plt
+import io
+import base64
+
 
 def pred():
     """
@@ -32,6 +35,24 @@ def pred():
 
     img_rgb_reconstructed = tfio.experimental.color.lab_to_rgb(img_lab_reconstructed)
 
+    #Convert into bytes and back to image
+    img_rgb_reconstructed = np.squeeze(img_rgb_reconstructed, axis=0)
+    # img_rgb_reconstructed_bytes = img_rgb_reconstructed.tobytes()
+    # img_rgb_reconstructed_processed =  Image.frombytes('RGB',
+    #                                      IMAGE_SIZE,
+    #                                      img_rgb_reconstructed_bytes)
+    # print(img_rgb_reconstructed)
+    img_rgb_reconstructed_processed = Image.fromarray(np.uint8(img_rgb_reconstructed*255))
+
+    #Reconstructed color image in PNG format
+    img_byte_arr = io.BytesIO()
+    img_rgb_reconstructed_processed.save(img_byte_arr, format='PNG')
+    img_byte_arr = img_byte_arr.getvalue()
+    img_reconstructed_base64 = base64.b64encode(img_byte_arr).decode('utf-8')
+
+    img_data = base64.b64decode(img_reconstructed_base64)
+    img_reconstructed = Image.open(io.BytesIO(img_data))
+
     print("\n✅ prediction done, plotting images")
 
     fig, ax = plt.subplots(1, 3, figsize=(10, 10))
@@ -40,7 +61,8 @@ def pred():
     ax[0].axis('off')
     ax[0].set_title('Input')
 
-    ax[1].imshow(np.squeeze(img_rgb_reconstructed,axis=0))
+    # ax[1].imshow(np.squeeze(img_rgb_reconstructed,axis=0))
+    ax[1].imshow(img_reconstructed)
     ax[1].axis('off')
     ax[1].set_title('Predicted')
 
